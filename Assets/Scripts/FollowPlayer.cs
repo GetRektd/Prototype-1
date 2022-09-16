2005a897df2dc1198a95124821272662b960a453
GetRektd <lgibb24@aoiths.org>
2022-09-16 13:04:01 -0400

    1.3

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -15,7 +15,7 @@ public class NewBehaviourScript : MonoBehaviour
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
 
-    void Update()
+     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
     }
